5e2fc0b9bdf204a4b5cfd5bf0aa0869d4455506e
SCWFoxy <121032351+SCWFoxy@users.noreply.github.com>
2022-12-19 12:57:16 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 // Comment Line
+// Second Comment Line
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
     res.send(`Hello Guest`)
