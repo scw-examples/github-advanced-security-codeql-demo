347f06b7995df9279d7386d86ec07791574e85fb
esioony <121180201+esioony@users.noreply.github.com>
2022-12-22 09:57:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
-    res.send(`Hello Guest`)
+    res.send(`Hello ${req.params.id}`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
