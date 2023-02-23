d5f6803012a3db5b63ce5039141f1ccb5bb0ede3
Trevor Hess <trevor.ghess@gmail.com>
2023-02-23 12:39:28 -0600

    Feature update - bad

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
-    res.send(`Hello Guest`)
+    eval('console.log("something else", ' + req.params.id + ')')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
